d7e7d6e062fdd812ab2e2c611fd6add45c7f9ecb
zzmp <zmp@umich.edu>
2014-01-12 07:40:31 -0600

    Bidding JS and Counting framework excepting bags

--- scripts/script.js
+++ scripts/script.js
@@ -37,12 +37,92 @@ function check(id) {
   document.getElementById(id).checked = true;
 }
 
-function setBids() {
+var bids = {};
 
+function setBids() {
+  var hL = Number(document.getElementById('homeBidLeft').value);
+  var hR = Number(document.getElementById('homeBidRight').value);
+  var aL = Number(document.getElementById('awayBidLeft').value);
+  var aR = Number(document.getElementById('awayBidRight').value);
+
+  console.log(hL,hR,aL,aR);
+
+  if ((hL + hR) > 13 || (aL + aR) > 13) {
+    alert('Team bids cannot exceed 13, please rebid');
+    return;
+  } else if (hL < 0 || hR < 0 || aL < 0 || aR < 0) {
+    alert('Bids must be at least nil (0), please rebid');
+    return;
+  }
+
+  bids.set = true;
+
+  if (hL + hR + aL + aR > 13)
+    alert('Someone\'s not going to make it!');
+
+  if (!(hL && hR))
+    bids.hNil = true;
+  if (!(aL && aR))
+    bids.aNil = true;
+
+  bids.h = hL + hR;
+  bids.a = aL + aR;
+
+  set('homeBid', bids.hNil ? bids.h + 'N' : bids.h);
+  set('awayBid', bids.aNil ? bids.a + 'N' : bids.a);
 }
 
-function setScore() {
+var score = {h: 0, a: 0, hBags: 0, aBags: 0};
 
+function setScore() {
+  function bagged(score, bags) {}
+
+  var h = Number(document.getElementById('homeTricks').value);
+  var hNil = document.getElementById('homeNil').checked;
+  var a = Number(document.getElementById('awayTricks').value);
+  var aNil = document.getElementById('awayNil').checked;
+
+  if (a+h !== 13) {
+    alert('There should be 13 tricks, pleease recount');
+    return;
+  }
+
+  h = h - bids.h;
+  a = a - bids.a;
+
+  if (h < 0)
+    score.h -= bids.h * 10;
+  else {
+    score.h += bids.h * 10;
+    bagged(score.h, h);
+  }
+  if (a < 0)
+    score.a -= bids.a * 10;
+  else {
+    score.a += bids.a * 10;
+    bagged(score.a, a)
+  }
+
+  if (bids.hNil) {
+    if (hNil)
+      score.h += 100;
+    else
+      score.h -= 100;
+  }
+  if (bids.aNil) {
+    if  (aNil)
+      score.a += 100;
+    else
+      score.a -= 100;
+  }
+
+  set('homeScore', score.h);
+  set('awayScore', score.a);
+
+  // reset
+  set('homeBid', '-');
+  set('awayBid', '-');
+  bids = {};
 }
 
 function clearBids() {
