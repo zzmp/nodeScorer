655ea987e44e173850128d6eae8ac6b7defc487f
zzmp <zmp@umich.edu>
2014-01-12 01:39:34 -0600

    Working JS

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,28 @@ init = function init() {
   document.getElementById('homeBid').innerHTML = '-';
   document.getElementById('awayBid').innerHTML = '-';
   document.getElementById('instructions').style.visibility = 'hidden';
+
+  document.getElementById('submitBid').onclick = function() {
+    var sBid = prompt('Home team: enter your bids','Enter bids separated by a comma. Nil and blind nil are denoted by 0, 00 (ex. 9,0 for 9 and nil)');
+    // validation would go here
+    document.getElementById('homeBid').innerHTML = sBid;
+    sBid = prompt('Away team: enter your bids','Enter bids separated by a comma. Nil and blind nil are denoted by 0, 00 (ex. 9,0 for 9 and nil)');
+    // validation would go here
+    document.getElementById('awayBid').innerHTML = sBid;
+  };
+
+  document.getElementById('submitScore').onclick = function() {
+    function newScore(oldScore) {
+      return iTricks + oldScore;
+    }
+    var iTricks = prompt('Home team: how many tricks did you take','If you broke nil, add an X (ex. 9X)');
+    // validation would go here
+    document.getElementById('homeScore').innerHTML = newScore(document.getElementById('homeScore').innerHTML);
+
+    iTricks = prompt('Away team: how many tricks did you take','If you broke nil, add an X (ex. 9X)');
+    // validation would go here
+    document.getElementById('awayScore').innerHTML = newScore(document.getElementById('awayScore').innerHTML);
+  };
 }
 
 function showInstructions() {	
@@ -26,26 +48,4 @@ function resetScore() {
 
 function showGames() {
   alert("Only Spades is available.");
-}
-
-document.getElementById('submitBid').onclick = function() {
-  var sBid = prompt('Home team: enter your bids','Enter bids separated by a comma. Nil and blind nil are denoted by 0, 00 (ex. 9,0 for 9 and nil)');
-  // validation would go here
-  document.getElementById('homeBid').innerHTML = sBid;
-  sBid = prompt('Away team: enter your bids','Enter bids separated by a comma. Nil and blind nil are denoted by 0, 00 (ex. 9,0 for 9 and nil)');
-  // validation would go here
-  document.getElementById('awayBid').innerHTML = sBid;
-}
-
-document.getElementById('submitScore').onclick = function() {
-  function newScore(oldScore) {
-    return iTricks + oldScore;
-  }
-  var iTricks = prompt('Home team: how many tricks did you take','If you broke nil, add an X (ex. 9X)');
-  // validation would go here
-  document.getElementById('homeScore').innerHTML = newScore(document.getElementById('homeScore').innerHTML);
-
-  iTricks = prompt('Away team: how many tricks did you take','If you broke nil, add an X (ex. 9X)');
-  // validation would go here
-  document.getElementById('awayScore').innerHTML = newScore(document.getElementById('awayScore').innerHTML);
 }
